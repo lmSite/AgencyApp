9b571f74a8eee2d6a74d08772cf221d0617e06c9
LM co <40068501+lmSite@users.noreply.github.com>
2018-10-29 19:22:09 +0200

    chg user

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -5,11 +5,11 @@ app.controller('loginCtrl', function ($scope, userSrv, $location, $rootScope) {
     $scope.invalidLogin = false;
 
     //user
-    //$scope.email="myplace@myplace.com";//TODO:remove for production
-    // $scope.password="123";//TODO:remove for production
+    $scope.email="myplace@myplace.com";//TODO:remove for production
+    $scope.password="123";//TODO:remove for production
 
-    $scope.email="dor@dor.com";//TODO:remove for production
-    $scope.password="444";//TODO:remove for production
+   // $scope.email="dor@dor.com";//TODO:remove for production
+   // $scope.password="444";//TODO:remove for production
 
     //   $scope.login(); //TODO:remove for production
 
